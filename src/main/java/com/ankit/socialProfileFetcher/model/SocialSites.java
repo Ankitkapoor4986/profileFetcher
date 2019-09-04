7798b093f1b39684518f2b2071a99e4cc03d79b3
ankitkapoor4986 <ankitkapoor4986@gmail.com>
2019-09-04 17:19:17 +0530

    Refactoring Social profiles

--- src/main/java/com/ankit/socialProfileFetcher/model/SocialSites.java
+++ src/main/java/com/ankit/socialProfileFetcher/model/SocialSites.java
@@ -1,16 +1,25 @@
 package com.ankit.socialProfileFetcher.model;
 
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
 public enum SocialSites {
 
-    FACEBOOK("facebook","www.facebook.com"),INSTAGRAM("instagram","www.instagram.com"),
-    PINTEST("pintest","www.pintest.com");
+    FACEBOOK("facebook", "www.facebook.com", (fb, socialProfileBuilder) ->
+            socialProfileBuilder.addFacebook(fb)),
+    INSTAGRAM("instagram", "www.instagram.com",
+            (insta, socialProfileBuilder) -> socialProfileBuilder.addInstagram(insta)),
+    PINTEST("pintest", "www.pintest.com",
+            (pin, socialProfileBuilder) -> socialProfileBuilder.addPinTest(pin));
 
     private final String url;
     private String siteName;
+    private BiConsumer<String, SocialProfileBuilder> socialProfileBuilderConsumer;
 
-    SocialSites(String siteName, String url) {
+    SocialSites(String siteName, String url, BiConsumer<String, SocialProfileBuilder> socialProfileBuilderConsumer) {
         this.siteName = siteName;
-        this.url= url;
+        this.url = url;
+        this.socialProfileBuilderConsumer = socialProfileBuilderConsumer;
     }
 
     public String getSiteName() {
@@ -19,5 +28,9 @@ public enum SocialSites {
 
     public String getUrl() {
         return url;
+    }
+
+    public BiConsumer<String, SocialProfileBuilder> getSocialProfileBuilderConsumer() {
+        return socialProfileBuilderConsumer;
     }
 }
--- src/main/java/com/ankit/socialProfileFetcher/scrapper/SocialProfileScrapper.java
+++ src/main/java/com/ankit/socialProfileFetcher/scrapper/SocialProfileScrapper.java
@@ -12,6 +12,8 @@ import org.jsoup.select.Elements;
 import java.util.List;
 import java.util.Optional;
 
+import static com.ankit.socialProfileFetcher.model.SocialSites.PINTEST;
+
 public class SocialProfileScrapper implements Scrapper<SocialProfilesHolder> {
 
     private static Scrapper<SocialProfilesHolder> scrapper = new SocialProfileScrapper();
@@ -36,28 +38,17 @@ public class SocialProfileScrapper implements Scrapper<SocialProfilesHolder> {
 
     private SocialProfilesHolder buildSocialProfileHolder(List<String> socialProfileLink) {
         SocialProfileBuilder socialProfilesBuilder = new SocialProfileBuilder();
-        addFbProfile(socialProfileLink, socialProfilesBuilder);
-        addInstaProfile(socialProfileLink, socialProfilesBuilder);
-        addPinTestProfile(socialProfileLink, socialProfilesBuilder);
+        addProfile(socialProfileLink, socialProfilesBuilder,SocialSites.FACEBOOK);
+        addProfile(socialProfileLink, socialProfilesBuilder,SocialSites.INSTAGRAM);
+        addProfile(socialProfileLink, socialProfilesBuilder,PINTEST);
         return new SocialProfilesHolder(socialProfilesBuilder.build(), true);
-
-
     }
 
-    private void addPinTestProfile(List<String> socialProfileLink, SocialProfileBuilder socialProfileBuilder) {
-        Optional<String> pintestProfileOptional = getSocialSiteUrl(socialProfileLink, SocialSites.PINTEST);
-        pintestProfileOptional.ifPresent(socialProfileBuilder::addPinTest);
+    private void addProfile(List<String> socialProfileLink, SocialProfileBuilder socialProfileBuilder, SocialSites socialSites) {
+        Optional<String> profileOptional = getSocialSiteUrl(socialProfileLink, socialSites);
+        profileOptional.ifPresent((pintestProfile)-> socialSites.getSocialProfileBuilderConsumer().accept(pintestProfile,socialProfileBuilder));
     }
 
-    private void addInstaProfile(List<String> socialProfileLink, SocialProfileBuilder socialProfileBuilder) {
-        Optional<String> instaProfileOp = getSocialSiteUrl(socialProfileLink, SocialSites.INSTAGRAM);
-        instaProfileOp.ifPresent(socialProfileBuilder::addInstagram);
-    }
-
-    private void addFbProfile(List<String> socialProfileLink, SocialProfileBuilder socialProfiles) {
-        Optional<String> fbUrlOptional = getSocialSiteUrl(socialProfileLink, SocialSites.FACEBOOK);
-        fbUrlOptional.ifPresent(socialProfiles::addFacebook);
-    }
 
     private Optional<String> getSocialSiteUrl(List<String> socialProfileLink, SocialSites socialSites) {
         return socialProfileLink.stream().filter(link -> socialSites.getSiteName().equals(link))
